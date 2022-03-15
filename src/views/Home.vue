f8d9ec54e8f0310274594aeeec17ff42c6903f74
Devjit Choudhury <devjitc1@gmail.com>
2022-03-15 11:07:43 +0530

    made card component and made Product page dynamic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,55 +12,29 @@
 
     <div class="recommended">
 
-      <div v-for="(product, i) in inventory.slice(0,3)" :key="product.id" class="card">
-        <div class="card-title">
-          {{ product.name }}
-        </div>
-        <div class="card-body">
-          <i class="icofont-10x icofont-{{ product.icon }}"></i>
-          <form>
-            <div class="row">
-              <div class="cell">
-                <label>Type:</label>
-              </div>
-              <div class="cell">
-                <em>{{ product.type }}</em>
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Price:</label>
-              </div>
-              <div class="cell">
-                ${{ product.price.USD }}
-              </div>
-            </div>
-            <div class="row">
-              <div class="cell">
-                <label>Quantity:</label>
-              </div>
-              <div class="cell">
-                <input type="number" v-model.numebr="product.quantity">
-              </div>
-            </div>
-          </form>
-        </div>
-        <div class="card-footer">
-          <button v-if="product.quantity > 0" @click="addToCart(product.name, i)" class="btn btn-light">Add to cart</button>
-        </div>
-      </div>
+        <ProductCard
+          v-for="(product, index) in inventory.slice(0,3)" 
+          :key="product.id"
+          class="card"
+          :index="index"
+          :product="product"
+          :addToCart="addToCart"
+        />
 
     </div>
+
 
   </main>
 </template>
 
 <script>
+import ProductCard from '@/components/ProductCard';
 
 export default {
   name: "Home-page",
   props: ['inventory', 'addToCart'],
   components: {
+    ProductCard
   },
 };
 </script>
